5e59355747600ad86d15c09251e584093358176c
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-06-27 21:26:06 +0200

    Question1

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1- Write a program that takes a number from
+            //the user then print yes if that number can be divided by 3 and 4 otherwise print no.
+            #region Question1
+            //int Num;
+            //Num=int.Parse(Console.ReadLine());
+            //if (Num % 3 == 0 && Num % 4 == 0)
+            //{
+            //    Console.WriteLine("yes");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("no");
+            //}
+            #endregion        }
         }
     }
 }
