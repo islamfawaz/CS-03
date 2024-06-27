8be09d905031670f1ea8eb09821959447eb748bd
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-06-27 21:29:12 +0200

    Question3

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -39,6 +39,62 @@
             // }
             #endregion
 
+
+
+            //3- Write a program that takes 3 integers from the user then prints
+            //the max element and the min element.
+            #region Question3
+            //int num1, num2, num3;
+            //num1=int.Parse(Console.ReadLine());
+            //num2 = int.Parse(Console.ReadLine());
+            //num3 = int.Parse(Console.ReadLine());
+
+            //if (num1 >= num2 && num1 >= num3)
+            //    Console.WriteLine(num1);
+
+
+            //else if (num2 >= num1 && num2 >= num3)
+            //    Console.WriteLine($"max element = {num2}");
+
+
+            //else if (num3 >= num1 && num3 >= num2)
+            //    Console.WriteLine($"max element = {num3}");
+
+
+
+            //if (num1 <= num2 && num1 <= num3)
+            //    Console.WriteLine($"min element = {num1}");
+
+
+            //else if (num2 <= num1 && num2 <= num3)
+            //    Console.WriteLine($"min element = {num2}");
+
+
+            //else if (num3 <= num1 && num3 <= num2)
+            //    Console.WriteLine($"min element = {num3}");
+
+
+            //anoter way
+
+            //int num1, num2, num3;
+            //num1 = int.Parse(Console.ReadLine());
+            //num2 = int.Parse(Console.ReadLine());
+            //num3 = int.Parse(Console.ReadLine());
+
+            //int maxElement = Math.Max(num1, Math.Max(num2, num3));
+            //int minElement = Math.Min(num1, Math.Min(num2, num3));
+
+            //Console.WriteLine($"max element = {maxElement}");
+            //Console.WriteLine($"min element = {minElement}");
+
+
+
+            #endregion
+
+
+
+
+
         }
     }
 }
