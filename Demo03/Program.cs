c436bd6fbc56b60b13a49fc698e6022a429aad3d
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-06-28 05:14:29 +0200

    Video2

--- Demo03/Program.cs
+++ Demo03/Program.cs
@@ -27,7 +27,63 @@
             //{
             //    Console.WriteLine("The Entered Month Number is not in the 1st Quarter");
             //} 
+            //switch (monthNumber)
+            //{
+            //    case 1:
+            //        Console.WriteLine("Hello January");
+            //        break;
+            //    case 2:
+            //        Console.WriteLine("Hello February");
+            //        break;
+            //    case 3:
+            //        Console.WriteLine("Hello March");
+            //        break;
+            //    default:
+            //        Console.WriteLine("Invalid month number");
+            //        break;
+            // }
             #endregion
+
+            #region Video2
+            // Using if-else
+            //Console.Write("Please Enter Your Age: ");
+            //string input = Console.ReadLine();
+            //if (int.TryParse(input, out int Age))
+            //{
+            //    // If-else version
+            //    if (Age > 22)
+            //    {
+            //        Console.WriteLine("Your Age is Greater than 22");
+            //    }
+            //    else if (Age < 22)
+            //    {
+            //        Console.WriteLine("Your Age is Less than 22");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Your Age equals 22");
+            //    }
+
+            //    // Switch statement with relational patterns
+            //    switch (Age)
+            //    {
+            //        case > 22:
+            //            Console.WriteLine("Your Age is Greater than 22");
+            //            break;
+            //        case < 22:
+            //            Console.WriteLine("Your Age is Less than 22");
+            //            break;
+            //        default:
+            //            Console.WriteLine("Your Age equals 22");
+            //            break;
+            //    }
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Invalid input. Please enter a valid integer.");
+            //}
+            #endregion
+
         }
     }
 }
