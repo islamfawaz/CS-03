bfc8aa7a2a930818e2e0dc6a2a9bcceec45212c5
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-06-28 06:08:01 +0200

    Video9

--- Demo03/Program.cs
+++ Demo03/Program.cs
@@ -258,6 +258,24 @@
 
             //Console.WriteLine(Message);
             #endregion
+
+            #region Video9
+            //Console.WriteLine(1);
+            //Console.WriteLine(2);   
+            //Console.WriteLine(3);   
+            //Console.WriteLine(4);   
+            //    Console.WriteLine(5);
+            //    Console.WriteLine(6);
+            //Console.WriteLine(7);   
+            //Console.WriteLine(8);   
+            //    Console.WriteLine(9);
+            //    Console.WriteLine(10);
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine(i);
+            //}
+            #endregion
         }
     }
 }
