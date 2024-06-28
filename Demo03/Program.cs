d066aa5b66829538e83316afd0046bc2b48d9554
TECH VALLEY <TECH VALLEY@DESKTOP-CUMGKU2>
2024-06-28 04:36:28 +0200

    Video1

---
+++ Demo03/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Demo03
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Video1
+
+            // Parse the input to an integer
+            //int monthNumber;
+            //int.TryParse(Console.ReadLine(), out monthNumber);
+
+            // Conditional statements to determine the month name
+            //if (monthNumber == 1)
+            //{
+            //    Console.WriteLine("Hello January");
+            //}
+            //else if (monthNumber == 2)
+            //{
+            //    Console.WriteLine("Hello February");
+            //}
+            //else if (monthNumber == 3)
+            //{
+            //    Console.WriteLine("Hello March");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("The Entered Month Number is not in the 1st Quarter");
+            //} 
+            #endregion
+        }
+    }
+}
